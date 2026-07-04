f6ea8f81d3b53754a377ae396d2daf6b35b1673e
huangjian <wlddhj@163.com>
2026-07-04 20:25:11 +0800

    chore(web): 清理 components.d.ts 中未使用的 ElTimeline 声明
    unplugin-vue-components 自动重新生成时移除未被引用的
    ElTimeline / ElTimelineItem 类型声明。

--- apps/forge-web/src/components.d.ts
+++ apps/forge-web/src/components.d.ts
@@ -66,8 +66,6 @@ declare module 'vue' {
     ElTabs: typeof import('element-plus/es')['ElTabs']
     ElTag: typeof import('element-plus/es')['ElTag']
     ElText: typeof import('element-plus/es')['ElText']
-    ElTimeline: typeof import('element-plus/es')['ElTimeline']
-    ElTimelineItem: typeof import('element-plus/es')['ElTimelineItem']
     ElTooltip: typeof import('element-plus/es')['ElTooltip']
     ElTree: typeof import('element-plus/es')['ElTree']
     ElTreeSelect: typeof import('element-plus/es')['ElTreeSelect']
